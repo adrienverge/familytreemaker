438f05f01a2b7c668003798123c162483abec382
Sasa Buklijas <buklijas.sasa@gmail.com>
2023-05-25 12:31:30 +0200

    separated directories for test files

--- tests/test.py
+++ tests/test.py
@@ -3,17 +3,21 @@ import os
 from pathlib import Path
 
 generated_file = 'generated.txt'
+input_dir = 'input'
 # input test files are called i_n.txt
     # where n is number of test
-for input_file in Path.cwd().glob("i_*.txt"):
+for input_file in Path.cwd().glob(f'{input_dir}{os.sep}i_*.txt'):
     input_file = input_file.name    # get just filename and convert to str
     print(f'TestCase: {input_file}')
 
     # expected output files are called o_n.txt
-    expected_file = 'o' + input_file[1:]
+    expected_file = f'expected_output{os.sep}o' + input_file[1:]
 
-    os.system(f'python ../familytreemaker.py {input_file} > {generated_file}')
+    # generate output file
+    os.system(f'python ..{os.sep}familytreemaker.py \
+                {input_dir}{os.sep}{input_file} > {generated_file}')
 
+    # check for differences between generated and expected output
     rv = os.system(f'diff {generated_file} {expected_file} >/dev/null')
     if rv == 0:
         print('    OK\n')
